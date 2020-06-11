af251448951d7b13f277cff0eba244209ad1c9c0
conni <cbuschka@gmail.com>
2020-06-11 12:55:38 +0200

    Added zipdiff.sh.

---
+++ zipdiff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=1.0.0
+ZIPDIFF_DIR=${HOME}/.zipdiff/${VERSION}
+ZIPDIFF_JAR=${ZIPDIFF_DIR}/zipdiff.jar
+
+mkdir -p ${ZIPDIFF_DIR}
+
+if [ ! -f "${ZIPDIFF_JAR}" ]; then
+  echo "Downloading zipdiff-${VERSION}..." >/dev/stderr
+  curl -sL -o /tmp/zipdiff.jar https://github.com/cbuschka/zipdiff/releases/download/zipdiff-${VERSION}/zipdiff.jar
+  mv /tmp/zipdiff.jar ${ZIPDIFF_JAR}
+fi
+
+java -jar ${ZIPDIFF_JAR} "$@"
